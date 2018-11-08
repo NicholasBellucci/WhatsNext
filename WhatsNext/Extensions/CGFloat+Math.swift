0aa8746049e5088c273f9418e6b6a5ede3265cae
Nick Bellucci <nickdbellucci@gmail.com>
2018-11-07 20:19:06 -0500

    Added snapping logic

---
+++ WhatsNext/Extensions/CGFloat+Math.swift
@@ -0,0 +1,19 @@
+//
+//  CGFloat+Math.swift
+//  WhatsNext
+//
+//  Created by Nicholas Bellucci on 11/7/18.
+//  Copyright © 2018 Nicholas Bellucci. All rights reserved.
+//
+
+import Foundation
+
+extension CGFloat {
+    func halved() -> CGFloat {
+        return self / 2
+    }
+
+    func minus(_ value: CGFloat) -> CGFloat {
+        return self - value
+    }
+}
